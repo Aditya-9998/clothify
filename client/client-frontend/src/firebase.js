07ee382381f3e6df5e2868013d44198c2d0655a9
Aditya Kumar <aditya.1994siwan@gmail.com>
2025-08-11 22:52:21 +0530

    Your commit message here

--- client/client-frontend/src/firebase.js
+++ client/client-frontend/src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBroSZ_LxVbjsHiv_W4gR2XTA3-ux1xoBs",
   authDomain: "clothify-auth-1765.firebaseapp.com",
   projectId: "clothify-auth-1765",
-  storageBucket: "clothify-auth-1765.appspot.com", // ✅ Correct bucket format
+  storageBucket: "clothify-auth-1765.firebasestorage.app",
   messagingSenderId: "338860292137",
   appId: "1:338860292137:web:17e201e890c1ad8b7c8438",
   measurementId: "G-YC6FB3V5NM"
